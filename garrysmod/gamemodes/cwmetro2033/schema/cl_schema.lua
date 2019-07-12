db260c0d31009fc3e5b44c6d1f5b219074ab13f5
The Lion <visiongamingjake@hotmail.com>
2019-07-12 15:49:30 +0200

    Updates cl_schema.lua

--- garrysmod/gamemodes/cwmetro2033/schema/cl_schema.lua
+++ garrysmod/gamemodes/cwmetro2033/schema/cl_schema.lua
@@ -4,32 +4,12 @@
 --]]
 
 Schema.stunEffects = {};
-Schema.combineOverlay = Material("effects/combine_binocoverlay");
-Schema.randomDisplayLines = {
-	"Transmitting physical transition vector...",
-	"Modulating external temperature levels...",
-	"Parsing view ports and data arrays...",
-	"Translating Union practicalities...",
-	"Updating biosignal co-ordinates...",
-	"Parsing Clockwork protocol messages...",
-	"Downloading recent dictionaries...",
-	"Pinging connection to network...",
-	"Updating mainframe connection...",
-	"Synchronizing locational data...",
-	"Translating radio messages...",
-	"Emptying outgoing pipes...",
-	"Sensoring proximity...",
-	"Pinging loopback...",
-	"Idle connection..."
-};
 
 Clockwork.config:AddToSystem("ServerWhitelistIdentity", "server_whitelist_identity", "ServerWhitelistIdentityDesc");
-Clockwork.config:AddToSystem("CombineLockOverrides", "combine_lock_overrides", "CombineLockOverridesDesc");
 Clockwork.config:AddToSystem("SmallIntroText", "intro_text_small", "SmallIntroTextDesc");
 Clockwork.config:AddToSystem("BigIntroText", "intro_text_big", "BigIntroTextDesc");
 Clockwork.config:AddToSystem("KnockoutTime", "knockout_time", "KnockoutTimeDesc", 0, 7200);
 Clockwork.config:AddToSystem("BusinessCost", "business_cost", "BusinessCostDesc");
-Clockwork.config:AddToSystem("CWUPropsEnabled", "cwu_props", "CWUPropsEnabledDesc");
 Clockwork.config:AddToSystem("PermitsEnabled", "permits", "PermitsEnabledDesc");
 Clockwork.config:AddToSystem("DullVision", "dull_vision", "DullVisionDesc");
 
@@ -61,34 +41,6 @@ Clockwork.datastream:Hook("Frequency", function(data)
 	end);
 	
 	if (!Clockwork.menu:GetOpen()) then
-		gui.EnableScreenClicker(true);
-	end;
-end);
-
-Clockwork.datastream:Hook("EditObjectives", function(data)
-	if (Schema.objectivesPanel and Schema.objectivesPanel:IsValid()) then
-		Schema.objectivesPanel:Close();
-		Schema.objectivesPanel:Remove();
-	end;
-	
-	Schema.objectivesPanel = vgui.Create("cwObjectives");
-	Schema.objectivesPanel:Populate(data or "");
-	Schema.objectivesPanel:MakePopup();
-	
-	gui.EnableScreenClicker(true);
-end);
-
-Clockwork.datastream:Hook("EditData", function(data)
-	if (IsValid(data[1])) then
-		if (Schema.dataPanel and Schema.dataPanel:IsValid()) then
-			Schema.dataPanel:Close();
-			Schema.dataPanel:Remove();
-		end;
-		
-		Schema.dataPanel = vgui.Create("cwData");
-		Schema.dataPanel:Populate(data[1], data[2] or "");
-		Schema.dataPanel:MakePopup();
-		
 		gui.EnableScreenClicker(true);
 	end;
 end);
@@ -158,55 +110,10 @@ Clockwork.chatBox:RegisterClass("request", "ic", function(info)
 	Clockwork.chatBox:Add(info.filtered, nil, unpack(localized));
 end);
 
--- A function to get a player's scanner entity.
-function Schema:GetScannerEntity(player)
-	local scannerEntity = Entity(player:GetSharedVar("Scanner"));
-	
-	if (IsValid(scannerEntity)) then
-		return scannerEntity;
-	end;
-end;
-
 -- A function to get whether a text entry is being used.
 function Schema:IsTextEntryBeingUsed()
 	if (self.textEntryFocused) then
 		if (self.textEntryFocused:IsValid() and self.textEntryFocused:IsVisible()) then
-			return true;
-		end;
-	end;
-end;
-
--- A function to add a Combine display line.
-function Schema:AddCombineDisplayLine(text, color)
-	if (self:PlayerIsCombine(Clockwork.Client)) then
-		if (!self.combineDisplayLines) then
-			self.combineDisplayLines = {};
-		end;
-		
-		table.insert(self.combineDisplayLines, {"<:: "..T(text), CurTime() + 8, 5, color});
-	end;
-end;
-
--- A function to get whether a player is Combine.
-function Schema:PlayerIsCombine(player, bHuman)
-	if (!IsValid(player)) then
-		return;
-	end;
-
-	local faction = player:GetFaction();
-	
-	if (self:IsCombineFaction(faction)) then
-		if (bHuman) then
-			if (faction == FACTION_MPF) then
-				return true;
-			end;
-		elseif (bHuman == false) then
-			if (faction == FACTION_MPF) then
-				return false;
-			else
-				return true;
-			end;
-		else
 			return true;
 		end;
 	end;
