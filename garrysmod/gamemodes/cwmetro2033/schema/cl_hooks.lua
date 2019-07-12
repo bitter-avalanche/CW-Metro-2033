983d0e09c0d47e3bacfbf43ce4d9391e55955386
The Lion <visiongamingjake@hotmail.com>
2019-07-12 15:51:13 +0200

    Update cl_hooks.lua

--- garrysmod/gamemodes/cwmetro2033/schema/cl_hooks.lua
+++ garrysmod/gamemodes/cwmetro2033/schema/cl_hooks.lua
@@ -273,12 +273,6 @@ function Schema:GetPlayerScoreboardClass(player)
 	if (customClass != "") then
 		return customClass;
 	end;
-	
-	if (faction == FACTION_MPF) then
-		return "Civil Protection";
-	elseif (faction == FACTION_OTA) then
-		return "Overwatch Transhuman Arm";
-	end;
 end;
 
 -- Called when the local player's character screen faction is needed.
@@ -436,17 +430,6 @@ function Schema:DrawTargetPlayerStatus(target, alpha, x, y)
 	end;
 end;
 
--- Called when the player info text is needed.
-function Schema:GetPlayerInfoText(playerInfoText)
-	local citizenID = Clockwork.Client:GetSharedVar("CitizenID");
-	
-	if (citizenID) then
-		if (Clockwork.Client:GetFaction() == FACTION_CITIZEN) then
-			playerInfoText:Add("CITIZEN_ID", L("InfoCitizenID", citizenID));
-		end;
-	end;
-end;
-
 -- Called to check if a player does have an flag.
 function Schema:PlayerDoesHaveFlag(player, flag)
 	if (!Clockwork.config:Get("permits"):Get()) then
@@ -466,74 +449,6 @@ end;
 function Schema:PlayerDoesRecognisePlayer(player, status, isAccurate, realValue)
 	if (self:PlayerIsCombine(player) or player:GetFaction() == FACTION_ADMIN) then
 		return true;
-	end;
-end;
-
--- Called each tick.
-function Schema:Tick()
-	if (IsValid(Clockwork.Client)) then
-		if (self:PlayerIsCombine(Clockwork.Client)) then
-			local curTime = CurTime();
-			local health = Clockwork.Client:Health();
-			local armor = Clockwork.Client:Armor();
-
-			if (!self.nextHealthWarning or curTime >= self.nextHealthWarning) then
-				if (self.lastHealth) then
-					if (health < self.lastHealth) then
-						if (health == 0) then
-							self:AddCombineDisplayLine("ErrorShuttingDown", Color(255, 0, 0, 255));
-						else
-							self:AddCombineDisplayLine("PhysicalTraumaDetected", Color(255, 0, 0, 255));
-						end;
-						
-						self.nextHealthWarning = curTime + 2;
-					elseif (health > self.lastHealth) then
-						if (health == 100) then
-							self:AddCombineDisplayLine("PhysicalSystemsRestored", Color(0, 255, 0, 255));
-						else
-							self:AddCombineDisplayLine("PhysicalSystemsRegenerate", Color(0, 0, 255, 255));
-						end;
-						
-						self.nextHealthWarning = curTime + 2;
-					end;
-				end;
-				
-				if (self.lastArmor) then
-					if (armor < self.lastArmor) then
-						if (armor == 0) then
-							self:AddCombineDisplayLine("ExternalProtectionExhausted", Color(255, 0, 0, 255));
-						else
-							self:AddCombineDisplayLine("ExternalProtectionDamaged", Color(255, 0, 0, 255));
-						end;
-						
-						self.nextHealthWarning = curTime + 2;
-					elseif (armor > self.lastArmor) then
-						if (armor == 100) then
-							self:AddCombineDisplayLine("ExternalProtectionRestored", Color(0, 255, 0, 255));
-						else
-							self:AddCombineDisplayLine("ExternalProtectionRegenerate", Color(0, 0, 255, 255));
-						end;
-						
-						self.nextHealthWarning = curTime + 2;
-					end;
-				end;
-			end;
-			
-			if (!self.nextRandomLine or curTime >= self.nextRandomLine) then
-				local text = self.randomDisplayLines[ math.random(1, #self.randomDisplayLines) ];
-				
-				if (text and self.lastRandomDisplayLine != text) then
-					self:AddCombineDisplayLine(text);
-					
-					self.lastRandomDisplayLine = text;
-				end;
-				
-				self.nextRandomLine = curTime + 3;
-			end;
-			
-			self.lastHealth = health;
-			self.lastArmor = armor;
-		end;
 	end;
 end;
 
